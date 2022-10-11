50dfa23bca4712ebb9d882456045bfaead1bfde5
matoiii2 <88582681+matoiii2@users.noreply.github.com>
2022-10-12 02:36:35 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+/*ナビゲーション*/
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', function() {
+     this.classList.toggle('toggle');
+});
+
+var mySwiper = new Swiper ('.swiper-container', {
+  effect: "flip",
+  loop: true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})
+
+var mySwiper = new Swiper ('.swiper-container', {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  centeredSlides : true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    767: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    }
+  }
+})
+
+$(function(){
+	$(window).on('load scroll',function (){
+		$('.animation').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+});
+
+$(function() {
+	setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
